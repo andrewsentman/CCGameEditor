9d755fa8d5f6eb9f649634dac0d2da12a1b87932
Andrew Sentman <asentman.s6.school@gmail.com>
2015-09-23 10:47:08 -0700

    Initial Commit

---
+++ desktop/src/com/qwertyzzz18/ccgame/editor/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.qwertyzzz18.ccgame.editor.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.qwertyzzz18.ccgame.editor.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
